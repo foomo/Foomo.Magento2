084843dd7cd6f8d897d9d5e6622d26d705fc4a26
Bostjan Marusic <bostjan@bestbytes.de>
2020-02-10 15:26:10 +0100

    Initial commit

---
+++ lib/Foomo/Magento2/DomainConfig.php
@@ -0,0 +1,15 @@
+<?php
+/*
+ */
+
+namespace Foomo\Magento2;
+
+
+class DomainConfig extends \Foomo\Config\AbstractConfig {
+
+	const NAME = 'Foomo.Magento2';
+	/**
+	 * @var string
+	 */
+	public $magentoRootFolder = '';
+}
